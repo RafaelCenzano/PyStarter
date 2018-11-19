69e8873215a4e550f410a800926ba6fa80955329
Savagecoder77 <savagecoder77@gmail.com>
2018-11-18 21:34:55 -0800

    change license

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ setup(
         'Topic :: Software Development :: Build Tools',
 
         # Pick your license as you wish
-        'License :: OSI Approved :: Apache 2.0',
+        'License :: OSI Approved :: MIT License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
