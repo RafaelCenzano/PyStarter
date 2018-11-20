a3e564590ece4b790840f508aad21720be1c5bd4
Savagecoder77 <savagecoder77@gmail.com>
2018-11-19 21:17:45 -0800

    cleaned up

--- setup.py
+++ setup.py
@@ -1,17 +1,6 @@
-"""
-A setuptools based setup module.
-See:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
-"""
-
-# Always prefer setuptools over distutils
 from setuptools import setup, find_packages
 from os import path
 # io.open is needed for projects that support Python 2.7
-# It ensures open() defaults to text mode with universal newlines,
-# and accepts an argument to specify the text encoding
-# Python 3 only projects can skip this import
 from io import open
 
 here = path.abspath(path.dirname(__file__))
@@ -20,50 +9,29 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
-# Arguments marked as "Required" below must be included for upload to PyPI.
-# Fields marked as "Optional" may be commented out.
-
 setup(
-    # This is the name of your project. The first time you publish this
-    # package, this name will be registered for you. It will determine how
-    # users can install this project, e.g.:
-    #
-    # $ pip install sampleproject
-    #
-    # And where it will live on PyPI: https://pypi.org/project/sampleproject/
-    #
-    # There are some restrictions on what makes a valid project name
-    # specification here:
-    # https://packaging.python.org/specifications/core-metadata/#name
+    # Name of package
     name='PyStarter',
 
-    # Versions complies with PEP 440:
-    # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.2',
+    # Version
+    version='0.1.3',
 
-    # This is a one-line description or tagline of what your project does. This
-    # corresponds to the "Summary" metadata field:
-    # https://packaging.python.org/specifications/core-metadata/#summary
+    # description
     description='A program to help you start python and git project files and venvs',
 
-    # This field corresponds to the "Description" metadata field:
-    # https://packaging.python.org/specifications/core-metadata/#description-optional
+    # Long description used as pypi homepage
     long_description=long_description,
 
-    # This field corresponds to the "Description-Content-Type" metadata field:
-    # https://packaging.python.org/specifications/core-metadata/#description-content-type-optional
+    # Stating long description is .rst
     long_description_content_type='text/x-rst',
 
-    # This field corresponds to the "Home-Page" metadata field:
-    # https://packaging.python.org/specifications/core-metadata/#home-page-optional
+    # Github home page
     url='https://github.com/SavageCoder77/PyStarter',
 
-    # This should be your name or the name of the organization which owns the
-    # project.
+    # Author
     author='Rafael Cenzano',
 
-    # This should be a valid email address corresponding to the author listed
-    # above.
+    # Contact info
     author_email='savagecoder77@gmail.com',
 
     # Classifiers help users find your project by categorizing it.
@@ -92,36 +60,16 @@ setup(
         'Programming Language :: Python :: 3.7',
     ],
 
-    # This field adds keywords for your project which will appear on the
-    # project page. What does your project relate to?
-    #
-    # Note that this is a string of words separated by whitespace, not a list.
+    # Keywords/Tags
     keywords='pystarter git python venv virtualenv',
 
-    # You can just specify package directories manually here if your project is
-    # simple. Or you can use find_packages().
-    #
-    # Alternatively, if you just want to distribute a single Python file, use
-    # the `py_modules` argument instead as follows, which will expect a file
-    # called `my_module.py` to exist:
-    #
-    #   py_modules=["my_module"],
-    #
+    # says what package your importing
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
 
-    # This field lists other packages that your project depends on to run.
-    # Any package you put here will be installed by pip when your project is
-    # installed, so they must be valid existing projects.
-    #
-    # For an analysis of "install_requires" vs pip's requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    # temp removed install_requires=[''],
+    # Needed installs
+    # install_requires=[''],
 
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.
-    #
-    # If using Python 2.6 or earlier, then these have to be included in
-    # MANIFEST.in as well.
+    # Data files
     #package_data={
     #    'sample': ['package_data.dat'],
     #},
@@ -133,15 +81,7 @@ setup(
         ],
     },
 
-    # List additional URLs that are relevant to your project as a dict.
-    #
-    # This field corresponds to the "Project-URL" metadata fields:
-    # https://packaging.python.org/specifications/core-metadata/#project-url-multiple-use
-    #
-    # Examples listed include a pattern for specifying where the package tracks
-    # issues, where the source is hosted, where to say thanks to the package
-    # maintainers, and where to support the project financially. The key is
-    # what's used to render the link text on PyPI.
+    # Additional links
     project_urls={
         'Bug Reports': 'https://github.com/SavageCoder77/PyStarter/issues',
         'Source': 'https://github.com/SavageCoder77/PyStarter',
