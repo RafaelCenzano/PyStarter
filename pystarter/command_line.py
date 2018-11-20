86a91e3692e03866ab19d21dcfbc2c4b456ee501
Savagecoder77 <savagecoder77@gmail.com>
2018-11-19 21:03:29 -0800

    add version command

--- pystarter/command_line.py
+++ pystarter/command_line.py
@@ -5,3 +5,8 @@ def main():
     args = sys.argv[1:]
     for arg in args:
         print('passed argument :: {}'.format(arg))
+
+    first_arg = sys.argv[1]
+
+    if first_arg == 'version' or first_arg == 'V':
+        print(pystarter.pystarterVersion())
--- pystarter/pystarter.py
+++ pystarter/pystarter.py
@@ -5,3 +5,6 @@ def versionFind(type='int'):
     version_tuple = sys.version_info
     print(version_tuple)
     print(type)
+
+def pystarterVersion():
+    return '0.1.2'
