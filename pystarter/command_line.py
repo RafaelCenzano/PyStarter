f4ebf2c90a05a68f9ef9b92ec80124596b8a80fa
Savagecoder77 <savagecoder77@gmail.com>
2018-11-25 14:04:06 -0800

    remove extra code

--- pystarter/command_line.py
+++ pystarter/command_line.py
@@ -3,9 +3,6 @@ import sys
 
 def main():
     args = sys.argv[1:]
-    for arg in args:
-        print('passed argument :: {}'.format(arg))
-
     first_arg = sys.argv[1]
 
     if first_arg == 'version' or first_arg == 'V':
