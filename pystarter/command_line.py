19bc71de42ef40562262006a777c23fe5ff3da0f
Savagecoder77 <savagecoder77@gmail.com>
2018-11-26 09:28:10 -0800

    add help command

--- pystarter/command_line.py
+++ pystarter/command_line.py
@@ -5,5 +5,11 @@ def main():
     args = sys.argv[1:]
     first_arg = sys.argv[1]
 
-    if first_arg == 'version' or first_arg == 'V':
+    if first_arg.lower() == '--version' or first_arg.lower() == '-v':
         print(pystarterVersion())
+
+    elif first_arg.lower() == '--help' or first_arg.lower() == '-h':
+        print(pystarterCommands())
+
+    else:
+        print(pystarterCommands())
