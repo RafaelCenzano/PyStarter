cea41758b97870dd31b7ae660859dfc047c1d1f3
Savagecoder77 <savagecoder77@gmail.com>
2018-11-26 09:28:17 -0800

    add command list

--- pystarter/pystarter.py
+++ pystarter/pystarter.py
@@ -1,6 +1,27 @@
 # Imports
 import sys
 
+
+commands = [
+            ' '
+            'Usage:'
+            '  pystarter <command> [options]'
+            ' '
+            'Commands:'
+            '  create             Create all needed files for a git and python project'
+            '                              default option creates git and python files'
+            ' '
+            'Command Options:'
+            '  python             Create project ready for python only'
+            '  git                Create project ready for git only'
+            ' '
+            'General Options:'
+            ' '
+            '  --help, -h         Show help'
+            '  --version, -v      Show PyStarter version'
+            ]
+
+
 def versionFind(type='int'):
     version_tuple = sys.version_info
     print(version_tuple)
@@ -8,3 +29,7 @@ def versionFind(type='int'):
 
 def pystarterVersion():
     return '0.1.5'
+
+def pystarterCommands():
+    long_string = '\n'.join(commands)
+    return long_string
