22e92d00beb4bd51b22ecc80537d2fee698acc89
Savagecoder77 <savagecoder77@gmail.com>
2018-11-27 18:04:25 -0800

    add .lower() and new create command

--- pystarter/command_line.py
+++ pystarter/command_line.py
@@ -3,13 +3,28 @@ import sys
 
 def main():
     args = sys.argv[1:]
-    first_arg = sys.argv[1]
+    first_arg = sys.argv[1].lower()
 
-    if first_arg.lower() == '--version' or first_arg.lower() == '-v':
+    if first_arg == '--version' or first_arg == '-v':
         print(pystarterVersion())
 
-    elif first_arg.lower() == '--help' or first_arg.lower() == '-h':
+    elif first_arg == '--help' or first_arg == '-h':
         print(pystarterCommands())
+
+    elif first_arg == 'create':
+        try:
+            second_arg = sys.argv[2].lower()
+        except ValueError:
+            second_arg = None
+
+        if second_arg == 'python':
+            pass
+        elif second_arg == 'git':
+            pass
+        elif second_arg == None:
+            pass
+        else:
+            pass
 
     else:
         print(pystarterCommands())
