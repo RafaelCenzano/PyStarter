957d77ff348e01d5c3a80c77094743962510f9c3
Savagecoder77 <savagecoder77@gmail.com>
2018-11-28 19:29:32 -0800

    move command list

--- pystarter/pystarter.py
+++ pystarter/pystarter.py
@@ -2,26 +2,6 @@
 import sys
 
 
-commands = ('''
-
-Usage:
-  pystarter <command> [options]
-
-Commands:'
-  create             Create all needed files for a git and python project
-                         default option creates git and python files
-
-Command Options:
-  python             Create project ready for python only
-  git                Create project ready for git only'
-
-'General Options:
-
-  --help, -h         Show help
-  --version, -v      Show PyStarter version
-            ''')
-
-
 def versionFind(type='int'):
     version_tuple = sys.version_info
     print(version_tuple)
@@ -31,4 +11,21 @@ def pystarterVersion():
     return '0.1.6'
 
 def pystarterCommands():
-    return commands
+    return ('''
+
+    Usage:
+      pystarter <command> [options]
+
+    Commands:'
+      create             Create all needed files for a git and python project
+                             default option creates git and python files
+
+    Command Options:
+      python             Create project ready for python only
+      git                Create project ready for git only'
+
+    'General Options:
+
+      --help, -h         Show help
+      --version, -v      Show PyStarter version
+                ''')
