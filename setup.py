79af1365819be1f85a7b52d367174fa3a6cae00f
Savagecoder77 <savagecoder77@gmail.com>
2018-12-02 11:10:50 -0800

    new version and Move to BETA

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     name='PyStarter',
 
     # Version
-    version='0.1.6',
+    version='1.0.0',
 
     # description
     description='A program to help you start python and git project files and venvs',
@@ -40,7 +40,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
