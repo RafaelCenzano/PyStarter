c1e77b92c3b028ec75a74713adee940649a86082
Savagecoder77 <savagecoder77@gmail.com>
2018-12-05 18:57:06 -0800

    remove uneeded functions

--- pystarter/test.py
+++ pystarter/test.py
@@ -3,6 +3,25 @@
 from pystarter import *
 import sys
 
+commands = ('''
+
+Usage:
+pystarter <command> [options]
+
+Commands:
+create             Create all needed files for a git and python project
+                     default option creates git and python files
+
+Command Options:
+python             Create project ready for python only
+git                Create project ready for git only
+
+General Options:
+
+--help, -h         Show help
+--version, -v      Show PyStarter version
+        ''')
+
 def main():
     #args = sys.argv[1:]
 
@@ -14,11 +33,11 @@ def main():
 
     # Version command
     if first_arg == '--version' or first_arg == '-v':
-        print(pystarterVersion())
+        print('1.1.1')
 
     # Help command
     elif first_arg == '--help' or first_arg == '-h':
-        print(pystarterCommands())
+        print(commands)
 
     # Main command to create needed files and things
     elif first_arg == 'create':
