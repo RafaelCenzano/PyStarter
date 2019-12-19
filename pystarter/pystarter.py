136ab53dbd51ef4c30c4abaf881623e6e4d3d898
Savagecoder77 <savagecoder77@gmail.com>
2019-12-18 23:31:48 -0800

    remove uneeded function

--- pystarter/pystarter.py
+++ pystarter/pystarter.py
@@ -1,35 +1,3 @@
-def versionFind(type='int'):
-    import platform
-
-    version = platform.python_version()
-    version_split = version.split('.')
-    main_version = version_split[0]
-    try:
-        sub_version = version_split[1]
-    except BaseException:
-        sub_version = None
-
-    try:
-        sub_sub_version = version_split[2]
-    except BaseException:
-        sub_sub_version = None
-
-    if type == 'int' or type == 'one' or type == 'first' or type == 1:
-        return main_version
-
-    elif type == 'second' or type == 'two' or type == 2:
-        return main_version, sub_version
-
-    elif type == 'third' or type == 'three' or type == 3:
-        return main_version, sub_version, sub_sub_version
-
-    elif type == 'str' or type == 'string':
-        return version
-
-    else:
-        raise MissingValue
-
-
 def findVenv():
     # import os
     try:
