9a5d44f1995ef325a913f72ffe7c0ffa78169547
Savagecoder77 <savagecoder77@gmail.com>
2019-12-20 02:12:53 -0800

    remove python 2 support

--- setup.py
+++ setup.py
@@ -51,8 +51,7 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
+        # Python 2 no support 2020 same with PyStarter!
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
