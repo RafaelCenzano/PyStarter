590208f956d4cd7bf1cdbcfa614a5d61ab5f9ab8
Savagecoder77 <savagecoder77@gmail.com>
2020-03-12 22:52:31 -0700

    prepare for v1.5

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     name='PyStarter',
 
     # Version
-    version='1.4.4',
+    version='1.5.0',
 
     # description
     description='A program to help you start python and git projects with file creations',
@@ -43,7 +43,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Stable',
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
