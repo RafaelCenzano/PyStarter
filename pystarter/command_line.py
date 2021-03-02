ac63da4f6589b1ecbbec837ec7bc03c57fec0906
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2021-03-02 01:07:29 -0800

    Use join on iterables on string

--- pystarter/command_line.py
+++ pystarter/command_line.py
@@ -582,9 +582,7 @@ pip3 install pystarter
                 exit()
 
     else:
-        passedArgs = ''
-        for items in sys.argv[1:]:
-            passedArgs += items
+        passedArgs = ' '.join(sys.argv[1:])
         print('Command ' + passedArgs + ' not found.\n')
         print(pystarterCommands())
 
